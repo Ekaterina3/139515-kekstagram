215ea00df6c5f9b100daba5a8a8bcf82dad0235d
Ekaterina Elesova <elesova.e@gmail.com>
2016-10-17 16:56:28 +0300

    Выполнено задание №6

---
+++ src/js/check.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function getMessage(a,b) {
+  if (typeof a == 'boolean') {
+    if (a == true) {
+      return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
+    } else {
+      return('Переданное GIF-изображение не анимировано');
+    }
+  } else if (typeof a == 'number') {
+      return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b*4 + ' атрибутов');
+  } else if ((typeof a == 'object') && !(typeof b == 'object')) {
+    var amountOfRedPoints = 0;
+    for (var i in a) {
+      amountOfRedPoints += a[i];
+    }
+    return('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
+  } else if ((typeof a == 'object') && (typeof b == 'object')) {
+    var artifactsSquare = 0;
+    for (var i in a) {
+      var j = 0;
+      artifactsSquare += a[i]*b[j];
+      j++;
+    }
+    return('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
+  } else {
+    return('Переданы некорректные данные');
+  }
+}
