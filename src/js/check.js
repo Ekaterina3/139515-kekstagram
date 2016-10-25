e167e785395bac158a97bd17501ceba50ae15636
Ekaterina Elesova <elesova.e@gmail.com>
2016-10-25 17:59:45 +0300

    правки

--- src/js/check.js
+++ src/js/check.js
@@ -1,25 +1,28 @@
 'use strict'
 
-function getMessage(a,b) {
-  if (typeof a == 'boolean') {
-    if (a == true) {
+function getMessage(a, b) {
+  var checkArrayA = Array.isArray(a);
+  var checkArrayB = Array.isArray(b);
+
+  if (typeof a === 'boolean') {
+    if (a === true) {
       return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
     } else {
       return('Переданное GIF-изображение не анимировано');
     }
-  } else if (typeof a == 'number') {
-      return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b*4 + ' атрибутов');
-  } else if ((typeof a == 'object') && !(typeof b == 'object')) {
+  } else if (typeof a === 'number') {
+      return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
+  } else if ((checkArrayA === true) && (checkArrayB === false)) {
     var amountOfRedPoints = 0;
-    for (var i in a) {
+    for (var i = 0; i < a.length; i++) {
       amountOfRedPoints += a[i];
     }
     return('Количество красных точек во всех строчках изображения: ' + amountOfRedPoints);
-  } else if ((typeof a == 'object') && (typeof b == 'object')) {
+  } else if ((checkArrayA === true) && (checkArrayB === true)) {
     var artifactsSquare = 0;
-    for (var i in a) {
+    for (var i = 0; i < a.length; i++) {
       var j = 0;
-      artifactsSquare += a[i]*b[j];
+      artifactsSquare += a[i] * b[j];
       j++;
     }
     return('Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей');
